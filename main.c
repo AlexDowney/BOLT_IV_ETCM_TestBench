46b56018748e6b53c5998922c685547f35e8ccac
AlexDowney <57879736+AlexDowney@users.noreply.github.com>
2020-02-27 15:15:26 -0500

    Wrote input prompts, trying to get reset check to work

--- main.c
+++ main.c
@@ -45,6 +45,7 @@ void init(void);
 void run(void);
 void initLookup(void);
 __interrupt void cpuTimer0ISR(void);
+void getInput(char* prompt, char* in, unsigned int length);
 bool Timer0Expired();
 
 void main(void)
@@ -61,66 +62,63 @@ void run(void)
     testState t = INPUT;
     buttonState outButton = UP;
     buttonState resetButton = UP;
-    char read[100];
-    char* newline = "\r\n";
+    char read[5];
     while (1)
     {
-        char* input = "Please enter a wheel speed (3 Digits):\t";
-        SCIwrite(SCI_DEBUG_BASE, (uint16_t *) input, strlen(input));
-        SCIread(SCI_DEBUG_BASE, (uint16_t *) read, 3);
-        SCIwrite(SCI_DEBUG_BASE, (uint16_t *) read, 3);
-        SCIwrite(SCI_DEBUG_BASE, (uint16_t *) newline, strlen(newline));
-        int bSwitch = atoi(read);
+        uint16_t suspensionTravel[2] = {0,0}; // {front, back}
+        uint16_t pSwitches[3] = {0,0,0};
+        uint16_t bSwitches[2] = {0,0};
+        uint16_t wSensors[2] = {0,0};
+        uint16_t tSwitch = 0;
+
+        getInput("Please enter a wheel speed (3 Digits):\t", read, 3);
+        wSensors[0] = atoi(read);
+        getInput("Please enter a wheel speed (3 Digits):\t", read, 3);
+        wSensors[1] = atoi(read);
+
+        getInput("Please enter a front wheel suspension value (3 Digits):\t", read, 3);
+        suspensionTravel[0] = atoi(read);
+        getInput("Please enter a back wheel suspension value (3 Digits):\t", read, 3);
+        suspensionTravel[1] = atoi(read);
+
+        getInput("Please enter a break value (1 or 0):\t", read, 1);
+        bSwitches[1] = atoi(read);
+        getInput("Please enter a break value (1 or 0):\t", read, 1);
+        bSwitches[1] = atoi(read);
+
+        getInput("Please enter a throttle (1 or 0):\t", read, 1);
+        tSwitch = atoi(read);
+
         //Should tactile buttons be used for Brake Switches, Profile Switches, and Throttle Closed Switch?
         //Wheel Speed sensor 2 pwm signals or just one?
         //I just need to send data that mimics the IMU not know how to use it right?
         //What measures suspension travel?
         //Whats the plan for a UI
 
-        uint16_t suspensionTravel[2] = {0,0};
-        uint16_t pSwitches[3] = {0,0,0};
-        uint16_t bSwitches[2] = {0,0};
-        uint16_t wSensors[2] = {0,0};
-        uint16_t tSwitch = 0;
+        GPIO_writePin(67, pSwitches[0]); //Profile Switches (J1 5,6,7)
+        GPIO_writePin(111, pSwitches[1]);
+        GPIO_writePin(60, pSwitches[2]);
 
-        buttonStateMachine(resetButtonStatus, &resetButton);
-        buttonStateMachine(outButtonStatus, &outButton);
+        GPIO_writePin(61, bSwitches[0]); //Brake Switches (J2 19, 18)
+        GPIO_writePin(123, bSwitches[1]);
 
-        switch(t)
+        GPIO_writePin(122, tSwitch); //Throttle Closed Switch (J2 17)
+
+        float request = (150 - suspensionTravel[0]) * (3.0/150);
+
+        setDACOutputVoltage(request); //ACTING AS SUSPENSION TRAVEL SENSOR HERE
+        //setDACOutputVoltage((150 - suspensionTravel[1]) * (5/150)); //ACTING AS SUSPENSION TRAVEL SENSOR HERE
+
+        //Calculate PWM Signal
+        //setCounterCompareAValue1();
+        //setCounterCompareAValue2();
+        char resetCheck[5];
+        char reset[5] = "reset";
+        while (strcmp(resetCheck, reset) != 0)
         {
-        case INPUT:
-            if (outButton == UP)
-            {
-                GPIO_writePin(67, pSwitches[0]); //Profile Switches (J1 5,6,7)
-                GPIO_writePin(111, pSwitches[1]);
-                GPIO_writePin(60, pSwitches[2]);
-
-                GPIO_writePin(61, bSwitches[0]); //Brake Switches (J2 19, 18)
-                GPIO_writePin(123, bSwitches[1]);
-
-                GPIO_writePin(122, tSwitch); //Throttle Closed Switch (J2 17)
-
-                float request = (150 - suspensionTravel[0]) * (3.0/150);
-
-                setDACOutputVoltage(request); //ACTING AS SUSPENSION TRAVEL SENSOR HERE
-                //setDACOutputVoltage((150 - suspensionTravel[1]) * (5/150)); //ACTING AS SUSPENSION TRAVEL SENSOR HERE
-
-                //Calculate PWM Signal
-                //setCounterCompareAValue1();
-                //setCounterCompareAValue2();
-
-                t = OUTPUT;
-            }
-            break;
-        case OUTPUT:
-            //Pull dac value from bike and verify with the inputs
-            //If it's right do something, if it's wrong do something
-            if (resetButton == DOWN)
-            {
-                //Clear outputs from test
-                t = INPUT;
-            }
-            break;
+            getInput("Type reset to reset:\t", resetCheck, 5);
+            int a = strcmp(resetCheck, reset);
+            int b = 1;
         }
     }
 }
@@ -166,6 +164,16 @@ __interrupt void cpuTimer0ISR(void)
     sampleGPIO(&resetButtonStatus);
     Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP1);
 }
+
+void getInput(char* prompt, char* in, unsigned int length){
+    char* newline = "\r\n";
+    SCIwrite(SCI_DEBUG_BASE, (uint16_t *) prompt, strlen(prompt));
+    SCIread(SCI_DEBUG_BASE, (uint16_t *) in, length);
+    SCIwrite(SCI_DEBUG_BASE, (uint16_t *) in, length);
+    SCIwrite(SCI_DEBUG_BASE, (uint16_t *) newline, strlen(newline));
+}
+
+
 //
 // End of File
 //
